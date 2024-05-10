57b193f37b5dc99ffe50fdeeae2f48a4866de305
Trang Le <trang.thule@zoho.com>
2024-05-10 20:33:06 +0700

    test fetching data

--- netlify/edge-functions/cat-trivia.ts
+++ netlify/edge-functions/cat-trivia.ts
@@ -4,6 +4,17 @@ export default async (request: Request, context: Context) => {
     // Get the page content
     const response = await context.next()
     const page = await response.text()
+    // regex for searching between blockquote tags
+    const catFactResponse = await fetch("https://meowfacts.herokuapp.com")
+    const json = await response.json()
+    console.log(json)
+    const fact = json.data[0]
+    const blockquoteRegex = /<blockquote id="blockquote.*>(?<text>.*?)<\/blockquote>/
+    const matched = blockquoteRegex.exec(page)
+    if (matched.groups && matched.groups["text"]) {
+        const blockquoteText = matched.groups["text"]
+        console.log({ blockquoteText })
+    }
     return new Response(page, {
         headers: {
             "Content-Type": "text/html",
