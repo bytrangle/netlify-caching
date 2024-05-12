20ee4ffbabeba154c3ae418b8c12ddd80dab61b3
Trang Le <trang.thule@zoho.com>
2024-05-12 17:06:57 +0000

    add edge function for modifying body in etag page

---
+++ netlify/edge-functions/etag.ts
@@ -0,0 +1,40 @@
+import crypto from "crypto"
+import type { Config, Context } from "@netlify/edge-functions"
+
+export default async (request: Request, context: Context) => {
+  const response = await context.next()
+  let body = await response.text()
+  const quotes = ["My dear, I'm a cat. Everything I see is mine.", "I don't really have owners. I have staff."]
+  const quote = Math.random() < 0.5 ? quotes[0] : quotes[1]
+  const quoteRegex = new RegExp(quote)
+  const quoteElementRegex = /(<blockquote.*>)(.+)(<\/blockquote>)/
+  const dateRegex = /(?<=<time.+>).+(?=<\/time>)/
+  // Only modify the response if the newly chosen quote is different from the last one
+  if (!body.match(quoteRegex).length) {
+    body = body.replace(quoteElementRegex, `$1${quote}$2`)
+    body = body.replace(dateRegex, new Date().toUTCString())
+  }
+  const etag = `${crypto.createHash("md5").update(body).digest("hex")}`
+  const headers = {
+    "Content-Type": "text/html",
+    "Cache-Control": "public, max-age=0, must-revalidate", // Tell browsers to always revalidate
+    "Netlify-CDN-Cache-Control": "public, max-age=31536000, must-revalidate",
+    "ETag": etag
+  }
+  if (request.headers["if-none-match"] === etag) {
+    return new Response(body, {
+      status: 304,
+      headers: headers
+    })
+  }
+  console.log("Regenerating quote")
+  return new Response(body, {
+    status: 200,
+    headers
+  })
+}
+
+export const config: Config = {
+  cache: "manual",
+  path: "/etag"
+}
