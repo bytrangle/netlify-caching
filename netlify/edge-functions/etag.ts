caa8bb4aedfd69c98367d4405d5cde46cdcc63a8
Trang Le <trang.thule@zoho.com>
2024-05-12 19:28:59 +0000

    try returning 200 status

--- netlify/edge-functions/etag.ts
+++ netlify/edge-functions/etag.ts
@@ -4,9 +4,10 @@ import type { Config, Context } from "@netlify/edge-functions"
 export default async (request: Request, context: Context) => {
   const response = await context.next()
   let body = await response.text()
-  // console.log(body)
+  console.log(body)
   const quotes = ["My dear, I'm a cat. Everything I see is mine.", "I don't really have owners. I have staff."]
   const quote = Math.random() < 0.5 ? quotes[0] : quotes[1]
+  console.log({ quote })
   const quoteRegex = new RegExp(quote)
   const quoteElementRegex = /(?<=<blockquote.+>).+(?=<\/blockquote>)/
   const dateRegex = /(?<=<time.+>).+(?=<\/time>)/
@@ -18,23 +19,23 @@ export default async (request: Request, context: Context) => {
     // console.log(updatedBody)
     updatedBody = updatedBody.replace(dateRegex, new Date().toUTCString())
   }
-  // console.log(updatedBody)
+  console.log(updatedBody)
   const etag = `${crypto.createHash("md5").update(updatedBody).digest("hex")}`
   const headers = {
     "Content-Type": "text/html",
     "Cache-Control": "public, max-age=0, must-revalidate", // Tell browsers to always revalidate
     "Netlify-CDN-Cache-Control": "public, max-age=31536000, must-revalidate",
-    "ETag": etag
+    // "ETag": etag
   }
-  console.log(`Request headers`, request.headers)
-  if (request.headers["if-none-match"] === etag) {
-    return new Response(updatedBody, {
-      status: 304,
-      headers
-    })
-  }
+  // if (request.headers["if-none-match"] === etag) {
+  //   return new Response(updatedBody, {
+  //     status: 304,
+  //     headers
+  //   })
+  // }
   console.log("Regenerating quote")
   return new Response(updatedBody, {
+    status: 200,
     headers
   })
 }
