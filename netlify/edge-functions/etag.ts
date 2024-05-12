de737033d1291ff877ec22b92745c80f62f9ff90
Trang Le <trang.thule@zoho.com>
2024-05-12 18:38:50 +0000

    insert quote into etag page

--- netlify/edge-functions/etag.ts
+++ netlify/edge-functions/etag.ts
@@ -5,17 +5,17 @@ export default async (request: Request, context: Context) => {
   const response = await context.next()
   let body = await response.text()
   console.log(body)
-  // const quotes = ["My dear, I'm a cat. Everything I see is mine.", "I don't really have owners. I have staff."]
-  // const quote = Math.random() < 0.5 ? quotes[0] : quotes[1]
-  // const quoteRegex = new RegExp(quote)
-  // const quoteElementRegex = /(<blockquote.*>)(.+)(<\/blockquote>)/
-  // const dateRegex = /(?<=<time.+>).+(?=<\/time>)/
+  const quotes = ["My dear, I'm a cat. Everything I see is mine.", "I don't really have owners. I have staff."]
+  const quote = Math.random() < 0.5 ? quotes[0] : quotes[1]
+  const quoteRegex = new RegExp(quote)
+  const quoteElementRegex = /(<blockquote.*>)(.+)(<\/blockquote>)/
+  const dateRegex = /(?<=<time.+>).+(?=<\/time>)/
   // // Only modify the response if the newly chosen quote is different from the last one
-  // if (!body.match(quoteRegex)) {
-  //   body = body.replace(quoteElementRegex, `$1${quote}$2`)
-  //   body = body.replace(dateRegex, new Date().toUTCString())
-  // }
-  // console.log(body)
+  if (!body.match(quoteRegex)) {
+    body = body.replace(quoteElementRegex, `$1${quote}$2`)
+    body = body.replace(dateRegex, new Date().toUTCString())
+  }
+  console.log(body)
   // const etag = `${crypto.createHash("md5").update(body).digest("hex")}`
   // const headers = {
   //   "Content-Type": "text/html",
