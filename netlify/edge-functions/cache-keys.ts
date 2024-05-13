9d2a4719d447395dce94039c7001edc3d59a371f
Trang Le <trang.thule@zoho.com>
2024-05-13 09:35:50 +0000

    log text

--- netlify/edge-functions/cache-keys.ts
+++ netlify/edge-functions/cache-keys.ts
@@ -2,8 +2,8 @@ import type { Config, Context } from "@netlify/edge-functions"
 
 export default async (request: Request, context: Context) => {
   const response = await context.next()
-  console.log(response)
   const text = await response.text()
+  console.log(text)
   return new Response(text, {
     headers: {
       "content-type": "text/html;charset=utf-8",
