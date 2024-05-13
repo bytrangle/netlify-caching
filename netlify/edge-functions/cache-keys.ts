9c1bd646603a1da7824905a90aa87990ba0f3803
Trang Le <trang.thule@zoho.com>
2024-05-13 08:52:34 +0000

    add charset to content-type

--- netlify/edge-functions/cache-keys.ts
+++ netlify/edge-functions/cache-keys.ts
@@ -7,7 +7,7 @@ export default async (request: Request, context: Context) => {
   let updatedText = text.replace(quoteElementRegex, "I don't really have owners. I have staff.")
   return new Response(updatedText, {
     headers: {
-      "content-type": "text/html",
+      "content-type": "text/html; chartset=utf-8",
       "Cache-Control": "public, max-age=0, must-revalidate", // Tell browsers to always revalidate
       "Netlify-CDN-Cache-Control": "public, max-age=31536000, must-revalidate",
     }
