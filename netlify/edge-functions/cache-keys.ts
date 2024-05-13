771b1ee7d845563b2a59721c351cc6cafba51ca7
Trang Le <trang.thule@zoho.com>
2024-05-13 09:19:05 +0000

    return text

--- netlify/edge-functions/cache-keys.ts
+++ netlify/edge-functions/cache-keys.ts
@@ -6,9 +6,9 @@ export default async (request: Request, context: Context) => {
   const text = await response.text()
   const quoteElementRegex = /(?<=<blockquote.+>).+(?=<\/blockquote>)/
   let updatedText = text.replace(quoteElementRegex, "I don't really have owners. I have staff.")
-  return new Response(updatedText, {
+  return new Response(text, {
     headers: {
-      "content-type": response.headers["content-type"],
+      "content-type": "text/html;charset=utf-8",
       "Cache-Control": "public, max-age=0, must-revalidate", // Tell browsers to always revalidate
       "Netlify-CDN-Cache-Control": "public, max-age=31536000, must-revalidate",
     }
