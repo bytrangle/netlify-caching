0e364bab5390805984c134367664b7affb01ee69
Trang Le <trang.thule@zoho.com>
2024-05-13 08:46:56 +0000

    replace block quote content

--- netlify/edge-functions/cache-keys.ts
+++ netlify/edge-functions/cache-keys.ts
@@ -3,7 +3,9 @@ import type { Config, Context } from "@netlify/edge-functions"
 export default async (request: Request, context: Context) => {
   const response = await context.next()
   const text = await response.text()
-  return new Response(text.toUpperCase(), {
+  const quoteElementRegex = /(?<=<blockquote.+>).+(?=<\/blockquote>)/
+  let updatedText = text.replace(quoteElementRegex, "I don't really have owners. I have staff.")
+  return new Response(updatedText, {
     headers: {
       "content-type": "text/html",
       "Cache-Control": "public, max-age=0, must-revalidate", // Tell browsers to always revalidate
