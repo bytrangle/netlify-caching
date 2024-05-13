894497c342799a2ab26b71eb55ebdda8057d089c
Trang Le <trang.thule@zoho.com>
2024-05-13 09:26:54 +0000

    remove logging of response

--- netlify/edge-functions/cache-keys.ts
+++ netlify/edge-functions/cache-keys.ts
@@ -2,7 +2,6 @@ import type { Config, Context } from "@netlify/edge-functions"
 
 export default async (request: Request, context: Context) => {
   const response = await context.next()
-  console.log(response)
   const text = await response.text()
   return new Response(text, {
     headers: {
