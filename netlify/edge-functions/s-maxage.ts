dd1317af7fca01bc1ecfd97b40cd034875d32ea2
Trang Le <trang.thule@zoho.com>
2024-05-16 22:36:31 +0700

    debug s-maxage

--- netlify/edge-functions/s-maxage.ts
+++ netlify/edge-functions/s-maxage.ts
@@ -5,20 +5,20 @@ export default async (request: Request, context: Context) => {
   if (response.status !== 200) return response
   const text = await response.text()
   console.log(text) // for debugging only
-  // return new Response(text, {
-  //   headers: {
-  //     "content-type": "text/html;charset=utf-8",
-  //     "Cache-Control": "public, max-age=0, must-revalidate", // Tell browsers to always revalidate
-  //     "Netlify-CDN-Cache-Control": "public, max-age=31536000, must-revalidate",
-  //   }
-  // })
-//      const response = await context.next()
-  const transformer = new TransformStream({
-    transform(chunk, controller) {
-      controller.enqueue(chunk.toString().replace(/MR_FRESH_QUOTE/g, "Blossom tea time"))
+  return new Response(text, {
+    headers: {
+      "content-type": "text/html;charset=utf-8",
+      "Cache-Control": "public, max-age=0, must-revalidate", // Tell browsers to always revalidate
+      "Netlify-CDN-Cache-Control": "public, max-age=31536000, must-revalidate",
     }
   })
-  return new Response(response.body.pipeThrough(transformer), response)
+//      const response = await context.next()
+  // const transformer = new TransformStream({
+  //   transform(chunk, controller) {
+  //     controller.enqueue(chunk.toString().replace(/MR_FRESH_QUOTE/g, "Blossom tea time"))
+  //   }
+  // })
+  // return new Response(response.body.pipeThrough(transformer), response)
 }
 
 export const config: Config = {
