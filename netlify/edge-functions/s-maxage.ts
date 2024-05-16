4625d56cd6697b77fc0e53d4b730da245fb2bae6
Trang Le <trang.thule@zoho.com>
2024-05-16 22:26:43 +0700

    change edge function name from cache-keys to max-age

---
+++ netlify/edge-functions/s-maxage.ts
@@ -0,0 +1,26 @@
+import type { Config, Context } from "@netlify/edge-functions"
+
+export default async (request: Request, context: Context) => {
+  const response = await context.next()
+  if (response.status !== 200) return response
+  const text = await response.text()
+  console.log(text) // for debugging only
+  // return new Response(text, {
+  //   headers: {
+  //     "content-type": "text/html;charset=utf-8",
+  //     "Cache-Control": "public, max-age=0, must-revalidate", // Tell browsers to always revalidate
+  //     "Netlify-CDN-Cache-Control": "public, max-age=31536000, must-revalidate",
+  //   }
+  // })
+//      const response = await context.next()
+  const transformer = new TransformStream({
+    transform(chunk, controller) {
+      controller.enqueue(chunk.toString().replace(/MR_FRESH_QUOTE/g, "Blossom tea time"))
+    }
+  })
+  return new Response(response.body.pipeThrough(transformer), response)
+}
+
+export const config: Config = {
+  path: "/s-maxage"
+}
