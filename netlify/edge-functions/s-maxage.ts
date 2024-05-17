685665da74d0479283708606967c6a8ff6508a33
Trang Le <trang.thule@zoho.com>
2024-05-17 16:56:31 +0700

    debug transformstream

--- netlify/edge-functions/s-maxage.ts
+++ netlify/edge-functions/s-maxage.ts
@@ -3,15 +3,26 @@ import type { Config, Context } from "@netlify/edge-functions"
 export default async (request: Request, context: Context) => {
   const response = await context.next()
   if (response.status !== 200) return response
-  const text = await response.text()
-  console.log(text) // for debugging only
+  // console.log(response)
+  // const text = await response.text()
+
+  // console.log(text) // for debugging only
 
   const transformer = new TransformStream({
     transform(chunk, controller) {
-      controller.enqueue(chunk.toLowerCase())
-  //     controller.enqueue(chunk.toString().replace(/MR_FRESH_QUOTE/g, "Blossom tea time"))
+      controller.enqueue(chunk.toUpperCase())
     }
   })
+  // // return new Response(response.body.pipeThrough(transformer), response)
+  // let updatedText = text.replace(/MR_FRESH_QUOTE/g, "Deep inside our wonderful world of vertical farms, freshness runs free, bland gets banished, and smart produce secures the future of food.")
+  //   return new Response(updatedText, {
+  //   headers: {
+  //     "content-type": "text/html;charset=utf-8",
+  //     "Cache-Control": "public, max-age=0, must-revalidate", // Tell browsers to always revalidate
+  //     "Netlify-CDN-Cache-Control": "public, max-age=31536000, must-revalidate",
+  //   }
+  // })
+
   return new Response(response.body.pipeThrough(transformer), response)
 }
 
